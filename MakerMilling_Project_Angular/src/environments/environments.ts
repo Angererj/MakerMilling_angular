cfc3660c34885793a922d6fb8dda703221fe2cf5
Angererj <71379043+Angererj@users.noreply.github.com>
2023-08-09 13:18:02 +0200

    small css bugfixes

--- MakerMilling_Project_Angular/src/environments/environments.ts
+++ MakerMilling_Project_Angular/src/environments/environments.ts
@@ -7,7 +7,7 @@ export const environment = {
   toolInSpindleImageEntity: "JA_SE.MakerMillingToolInSpindleImage.Media",
   programPreviewImageEntity: "JA_SE.MakerMillingActiveProgramImage.Media",
   liveCameraImageEntity: "JA_SE.MakerMillingLiveCameraFeedImage.Media",
-  userIsAuthenticated: true
+  userIsAuthenticated: false
 };
 
 //change user is authenticated to passed boolean value
